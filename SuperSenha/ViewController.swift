e925343d33c9b588e98a9a5f1edc164e985adfa1
GilmarMMJr <gilmar_mendonca@live.com>
2021-08-28 12:55:12 -0300

    I created the viewController outlets

--- SuperSenha/ViewController.swift
+++ SuperSenha/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalPasswordsTextField: UITextField!
+    @IBOutlet weak var numberOfCharactersTextField: UITextField!
+    @IBOutlet weak var lettersSwitch: UISwitch!
+    @IBOutlet weak var numbersSwitch: UISwitch!
+    @IBOutlet weak var capitalLettersSwitch: UISwitch!
+    @IBOutlet weak var specialCharactersSwich: UISwitch!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
